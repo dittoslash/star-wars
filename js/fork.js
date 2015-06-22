81b5bd4d3108b6881bdadb64a2a610946c7603cb
Henry <dittoslash@users.noreply.github.com>
2015-06-22 14:20:22 +0100

    Update fork.js

--- js/fork.js
+++ js/fork.js
@@ -5,7 +5,7 @@ Game.Fork = function(being, direction) {
 	Game.audio.play("fork");
 	this._force = new Game.Force(being, direction, 6);
 	this._force.setBeingMethod("damage", Game.Rules.FORK_DAMAGE, Game.Rules.FORK_DAMAGE);
-	this._force.offsetChars(100);
+	this._force.offsetChars(50);
 	this._force.setWaveOrder(2);
 	this._force.clearEachRow(false);
 	this._force.setDelay(75);
